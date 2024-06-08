b3a22174edac31913e981e1254b1b11ac354250a
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-06-08 11:41:57 -0300

    .

---
+++ ModelosAppProgramacion1/ModeloDeUnSoloFormularioConClaseControladora/Models/Controlador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDeUnSoloFormularioConClaseControladora.Models
+{
+    public class Controlador
+    {
+        public double acum;
+        public int contador;
+
+        public double CalcularPromedio()
+        {
+            double promedio = 0;
+            if (contador > 0)
+                promedio = acum / contador;
+            return promedio;
+        }
+
+        public void RegistrarNota(double nota)
+        {
+            acum += nota;
+            contador++;
+        }
+    }
+}
