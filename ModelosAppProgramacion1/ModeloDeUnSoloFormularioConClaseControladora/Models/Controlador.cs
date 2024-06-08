9cd0e1569f97c96b833e17dd8f585fe484b6737b
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-06-08 10:34:29 -0300

    emprolijado

--- ModelosAppProgramacion1/ModeloDeUnSoloFormularioConClaseControladora/Models/Controlador.cs
+++ ModelosAppProgramacion1/ModeloDeUnSoloFormularioConClaseControladora/Models/Controlador.cs
@@ -6,25 +6,23 @@ using System.Threading.Tasks;
 
 namespace ModeloDeUnSoloFormularioConClaseControladora.Models
 {
-    public class Controlador
+public class Controlador
+{
+    public double acum;
+    public int contador;
+
+    public double CalcularPromedio()
     {
-        #region variables y  métodos del dominio del problema
-        public double acum;
-        public int contador;
-
-        public double CalcularPromedio()
-        {
-            double promedio = 0;
-            if (contador > 0)
-                promedio = acum / contador;
-            return promedio;
-        }
-
-        public void RegistrarNota(double nota)
-        {
-            acum += nota;
-            contador++;
-        }
-        #endregion
+        double promedio = 0;
+        if (contador > 0)
+            promedio = acum / contador;
+        return promedio;
     }
+
+    public void RegistrarNota(double nota)
+    {
+        acum += nota;
+        contador++;
+    }
+}
 }
